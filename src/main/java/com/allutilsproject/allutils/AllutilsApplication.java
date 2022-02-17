9236602a88d11064989196085f02e6e90d463af0
Guilherme <guilherme.pc@yahoo.com>
2022-02-17 20:24:29 -0300

    first commit

---
+++ src/main/java/com/allutilsproject/allutils/AllutilsApplication.java
@@ -0,0 +1,13 @@
+package com.allutilsproject.allutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllutilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllutilsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allutilsproject/allutils/AllutilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allutilsproject.allutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllutilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
